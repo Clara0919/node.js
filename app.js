658882f4e8e2ccec937adefeeb6b0ee07be34bd4
Clara0919 <mansyuan.he@gmail.com>
2022-05-11 11:52:19 +0800

    1-3 使用NPM管理專案

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 console.log("hello I'm clara");
-const hello = require('./hello'); //匯入模組
+
+//內建模組
 const path = require('path'); //匯入模組
+
+//第三方模組
+
+//自建模組
+const hello = require('./hello'); //匯入模組
+//////////////////////////////////////////////////////////////////////////////////////////
+
 hello.sayGoodnight();
 console.log('dirname', __dirname);
 console.log('filename', __filename);
