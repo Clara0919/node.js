73a6d02aa41cbc5f169fa1537244ed6528870570
Clara0919 <mansyuan.he@gmail.com>
2022-05-11 13:36:43 +0800

    1-5 第三方套件 cowsay

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ console.log("hello I'm clara");
 const path = require('path'); //匯入模組
 
 //第三方模組
+const cowsay = require('cowsay');
+
 
 //自建模組
 const hello = require('./hello'); //匯入模組
@@ -13,3 +15,14 @@ hello.sayGoodnight();
 console.log('dirname', __dirname);
 console.log('filename', __filename);
 console.log(path.join(__dirname, 'index.js'));
+
+
+let sentences = ['Hello', 'World', 'I\'m a cow.'];
+
+sentences.forEach((sentence) => {
+    console.log(cowsay.say({
+        text: sentence,
+        e: "^^",
+        T: "U "
+    }));
+});
