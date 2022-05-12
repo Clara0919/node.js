b4a1e85cca35d46fe4e954801e8f408438d34767
Clara0919 <mansyuan.he@gmail.com>
2022-05-12 09:08:25 +0800

    3-1 使用 Express 創建 Web Server 並設定路由

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
-console.log("hello I'm clara");
+// 第一個區塊 內建模組
+const path = require('path');
 
-//內建模組
-const path = require('path'); //匯入模組
-const http = require('http');
-const url = require('url');
+// 第二個區塊 第三方模組(套件)
+const express = require('express');
 
-//第三方模組
+// 第三個區塊 自建模組
 
+////////////////////////////////////////////////////////////////
 
+const app = express();
 
-//自建模組
-const hello = require('./hello'); //匯入模組
-//////////////////////////////////////////////////////////////////////////////////////////
-
-hello.sayGoodnight();
-console.log('dirname', __dirname);
-console.log('filename', __filename);
-console.log(path.join(__dirname, 'index.js'));
-const server = http.createServer((req, res) => { //req:request res:response
-    // console.log('第一個參數是瀏覽器對 web server 的 request', req);
-    // console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
-    console.log('req url', req.url)
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        return res.end('<h1>This is home page</h1>');
-
-    }
-    if (req.url === '/login') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        return res.end('<h1>This is login page</h1>');
-    }
-    res.end();//代表請求結束 記得寫
+// middleware
+app.use((req, res, next) => {
+    console.log('Hello!');
+    next();
 });
 
-server.listen(3000, () => {
-    console.log('running server on port 3000');
+app.use((req, res, next) => {
+    console.log('World!');
+    next();
 });
 
-console.log(url.parse('https://www.youtube.com/watch?v=ssZTiB8yg94&t=43s'));//用來分析網址內含的狀態
+app.get('/', (req, res) => {
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('<head><meta charset="utf-8" /></head>')
+    // res.write('<body>')
+    // res.write('<h1>這是首頁</h1>')
+    // res.write('</body>')
+    res.status(200)
+        .sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
-
+app.listen(3000, () => {
+    console.log('Web Server is running on port 3000');
+});
